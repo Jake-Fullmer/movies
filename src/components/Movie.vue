56bf02685cc6b796898557a9caebe52dd4a19631
Jake-Fullmer <jnfu@outlook.com>
2020-02-26 22:07:35 -0700

    ...

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,5 +1,6 @@
 <template>
-  <li class="my-1" @click="setActive">
+  <li class="my-1">
+    <!-- @click="setActive -->
     <img
       v-if="movieData.poster_path"
       :src="'https://image.tmdb.org/t/p/w500/' + movieData.poster_path"
